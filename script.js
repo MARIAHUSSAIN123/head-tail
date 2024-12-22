7f24b09da4644b03b87ec1f25c2a6f3a0b2c3df6
Maria Hussain <162424488+MARIAHUSSAIN123@users.noreply.github.com>
2024-12-22 21:29:57 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+function coin(){
+    let flip=Math.floor(Math.random()*2);
+    if(flip===0){
+        document.getElementById("empty").innerHTML="Head :1";
+    }else{
+        document.getElementById("empty").innerHTML="Tail :0";
+    }
+}
